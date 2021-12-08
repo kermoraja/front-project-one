467c70a1e9add703249b5d5da237794bd7832a15
kermoraja <94180496+kermoraja@users.noreply.github.com>
2021-12-08 15:24:36 +0200

    Update Admin.vue

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -2,59 +2,52 @@
   <div>
     <v-main>
       <v-container>
-            <v-row
-                :align="align"
-                no-gutters
-                style="height: 150px;">
-              <v-col md="3" xs="12">
-                <v-text-field
-                    label="Tour Title"
-                    :rules="rules"
-                    hide-details="auto"
-                    v-model="input_tour.title"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row
-                :align="align"
-                no-gutters
-                style="height: 150px;">
-              <v-col md="3" xs="12">
-                <v-textarea
-                    name="input-7-1"
-                    label="Long description"
-                    value=""
-                    hint="Hint text"
-                    v-model="input_tour.desc_long">
+        <v-row
+            style="height: 150px;">
+          <v-col md="6" xs="12">
+            <v-text-field
+                label="Tour Title"
+                :rules="rules"
+                v-model="input_tour.title"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col md="6" xs="12">
+            <v-textarea
+                name="input"
+                label="Long description"
+                value=""
+                hint="Hint text"
+                v-model="input_tour.desc_long">
 
-                </v-textarea>
-              </v-col>
-            </v-row>
-            <v-row
-                :align="align"
-                no-gutters
-                style="height: 150px;">
-              <v-col md="3" xs="12">
-                <v-text-field v-model="input_tour.desc_short" :rules="rules" label="Short Description"></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col md="3" xs="12">
-                <v-text-field v-model="input_tour.duration" :rules="rules" label="Duration"></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col md="3" xs="12">
-                <v-text-field v-model="input_tour.city" :rules="rules" label="City"></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row justify="space-around">
-              <v-col v-col md="1" xs="12">
-                <v-btn depressed v-on:click = "createTour()">
-                  Submit
-                </v-btn>
-              </v-col>
-            </v-row>
+            </v-textarea>
+          </v-col>
+        </v-row>
+        <v-row
+            no-gutters
+            style="height: 150px;">
+          <v-col md="3" xs="12">
+            <v-text-field v-model="input_tour.desc_short" :rules="rules" label="Short Description"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col md="3" xs="12">
+            <v-text-field v-model="input_tour.duration" :rules="rules" label="Duration"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col md="3" xs="12">
+            <v-text-field v-model="input_tour.city" :rules="rules" label="City"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="space-around">
+          <v-col v-col md="1" xs="12">
+            <v-btn depressed v-on:click = "createTour()">
+              Submit
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
   </div>
@@ -91,9 +84,9 @@ export default {
             this.tour_created = response.data
             console.log(this.tour_created)
           })
-  }
+    }
 
-}
+  }
 }
 </script>
 <style>
