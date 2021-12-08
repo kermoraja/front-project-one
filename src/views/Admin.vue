997121bd83b2d0820a7a09af7b8fbf45a00d27e3
kermoraja <94180496+kermoraja@users.noreply.github.com>
2021-12-08 15:14:48 +0200

    Update Admin.vue

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -2,13 +2,11 @@
   <div>
     <v-main>
       <v-container>
-        <v-layout row wrap>
-          <v-flex xs12 md3>
             <v-row
                 :align="align"
                 no-gutters
                 style="height: 150px;">
-              <v-col>
+              <v-col md="3" xs="12">
                 <v-text-field
                     label="Tour Title"
                     :rules="rules"
@@ -17,76 +15,46 @@
                 ></v-text-field>
               </v-col>
             </v-row>
-          </v-flex>
-        </v-layout>
-        <v-layout>
-          <v-flex xs12 md3>
             <v-row
                 :align="align"
                 no-gutters
                 style="height: 150px;">
-              <v-col>
+              <v-col md="3" xs="12">
                 <v-textarea
                     name="input-7-1"
                     label="Long description"
                     value=""
                     hint="Hint text"
-                    v-model="input_tour.desc_long"
-                ></v-textarea>
+                    v-model="input_tour.desc_long">
+
+                </v-textarea>
               </v-col>
             </v-row>
-          </v-flex>
-        </v-layout>
-        <v-layout>
-          <v-flex xs12 md3>
             <v-row
                 :align="align"
                 no-gutters
                 style="height: 150px;">
-              <v-col>
+              <v-col md="3" xs="12">
                 <v-text-field v-model="input_tour.desc_short" :rules="rules" label="Short Description"></v-text-field>
               </v-col>
             </v-row>
-          </v-flex>
-        </v-layout>
-        <v-layout>
-          <v-flex xs12 md3>
             <v-row>
-              <v-col>
+              <v-col md="3" xs="12">
                 <v-text-field v-model="input_tour.duration" :rules="rules" label="Duration"></v-text-field>
               </v-col>
             </v-row>
-          </v-flex>
-        </v-layout>
-        <v-layout>
-          <v-flex xs12 md3>
             <v-row>
-              <v-col>
+              <v-col md="3" xs="12">
                 <v-text-field v-model="input_tour.city" :rules="rules" label="City"></v-text-field>
               </v-col>
             </v-row>
-          </v-flex>
-        </v-layout>
-<!--        <v-layout>-->
-<!--          <v-flex xs12 md3>-->
-<!--            <v-row>-->
-<!--              <v-col>-->
-<!--                <v-text-field v-model="input_tour.img_url" :rules="rules" label="Images"></v-text-field>-->
-<!--              </v-col>-->
-<!--            </v-row>-->
-<!--          </v-flex>-->
-<!--        </v-layout>-->
-        <v-layout>
-          <v-flex xs12 md1>
             <v-row justify="space-around">
-              <v-col>
+              <v-col v-col md="1" xs="12">
                 <v-btn depressed v-on:click = "createTour()">
                   Submit
                 </v-btn>
               </v-col>
             </v-row>
-          </v-flex>
-        </v-layout>
       </v-container>
     </v-main>
   </div>
@@ -104,16 +72,17 @@ export default {
       city:"",
       img_url:"",
       input_tour: {},
-      tour_created: ""
+      tour_created: "",
+      rules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'Min 3 characters',
+      ],
+      alignments: [
+        'center',
+      ],
     }
   },
-  rules: [
-    value => !!value || 'Required.',
-    value => (value && value.length >= 3) || 'Min 3 characters',
-  ],
-  alignments: [
-    'center',
-  ],
+
   methods:{
     createTour: function () {
 
