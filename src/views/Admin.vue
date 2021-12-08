087848a0e5a6f02af5859655ba1b2b155e138cb0
Kermo <raja.kermo@gmail.com>
2021-12-08 15:09:50 +0200

    admini leht

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -2,106 +2,98 @@
   <div>
     <v-main>
       <v-container>
-        <v-layout row wrap>
-          <v-flex xs12 md3>
-            <v-row
-                :align="align"
-                no-gutters
-                style="height: 150px;">
-              <v-col>
-                <v-text-field
-                    label="Tour Title"
-                    :rules="rules"
-                    hide-details="auto"
-                    v-model="title"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-          </v-flex>
-        </v-layout>
-        <v-layout>
-          <v-flex xs12 md3>
-            <v-row
-                :align="align"
-                no-gutters
-                style="height: 150px;">
-              <v-col>
-                <v-textarea
-                    name="input-7-1"
-                    label="Long description"
-                    value=""
-                    hint="Hint text"
-                ></v-textarea>
-              </v-col>
-            </v-row>
-          </v-flex>
-        </v-layout>
-        <v-layout>
-          <v-flex xs12 md3>
-            <v-row
-                :align="align"
-                no-gutters
-                style="height: 150px;">
-              <v-col>
-                <v-text-field v-model="desc_short" :rules="rules" label="Short Description"></v-text-field>
-              </v-col>
-            </v-row>
-          </v-flex>
-        </v-layout>
-        <v-layout>
-          <v-flex xs12 md3>
-            <v-row>
-              <v-col>
-                <v-text-field v-model="duration" :rules="rules" label="Duration"></v-text-field>
-              </v-col>
-            </v-row>
-          </v-flex>
-        </v-layout>
-        <v-layout>
-          <v-flex xs12 md3>
-            <v-row>
-              <v-col>
-                <v-text-field v-model="desc_short" :rules="rules" label="City"></v-text-field>
-              </v-col>
-            </v-row>
-          </v-flex>
-        </v-layout>
-        <v-layout>
-          <v-flex xs12 md3>
-            <v-row>
-              <v-col>
-                <v-text-field v-model="img_url" :rules="rules" label="Images"></v-text-field>
-              </v-col>
-            </v-row>
-          </v-flex>
-        </v-layout>
-        <v-layout>
-          <v-flex xs15 md1>
-            <v-row justify="space-around">
-              <v-col>
-                <v-btn depressed>
-                  Sumbit
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-flex>
-        </v-layout>
+        <v-row
+            :align="align"
+            no-gutters
+            style="height: 150px;">
+          <v-col md="3" xs="12">
+            <v-text-field
+                label="Tour Title"
+                :rules="rules"
+                hide-details="auto"
+                v-model="input_tour.title"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row
+            :align="align"
+            no-gutters
+            style="height: 150px;">
+          <v-col md="3" xs="12">
+            <v-textarea
+                name="input-7-1"
+                label="Long description"
+                value=""
+                hint="Hint text"
+                v-model="input_tour.desc_long">
+
+            </v-textarea>
+          </v-col>
+        </v-row>
+        <v-row
+            :align="align"
+            no-gutters
+            style="height: 150px;">
+          <v-col md="3" xs="12">
+            <v-text-field v-model="input_tour.desc_short" :rules="rules" label="Short Description"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col md="3" xs="12">
+            <v-text-field v-model="input_tour.duration" :rules="rules" label="Duration"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col md="3" xs="12">
+            <v-text-field v-model="input_tour.city" :rules="rules" label="City"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="space-around">
+          <v-col v-col md="1" xs="12">
+            <v-btn depressed v-on:click = "createTour()">
+              Submit
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
   </div>
 </template>
 <script>
-export default {
-  data: () => ({
-    rules: [
-      value => !!value || 'Required.',
-      value => (value && value.length >= 3) || 'Min 3 characters',
-    ],
-    alignments: [
-      'center',
-    ],
-  }),
+import router from "../router";
 
+export default {
+  data: function () {
+    return {
+      title:"",
+      desc_short:"",
+      desc_long:"",
+      duration:"",
+      city:"",
+      img_url:"",
+      input_tour: {},
+      tour_created: "",
+      rules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'Min 3 characters',
+      ],
+      alignments: [
+        'center',
+      ],
+    }
+  },
+
+  methods:{
+    createTour: function () {
+
+      this.$http.post('/api/tour', this.input_tour)
+          .then(response => {
+            this.tour_created = response.data
+            console.log(this.tour_created)
+          })
+    }
+
+  }
 }
 </script>
 <style>
